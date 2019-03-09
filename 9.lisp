0a97f7dfc69d80087e5df3aa5edbd5ed1e598517
SimfikDuke <simfikduke@gmail.com>
2019-03-09 12:47:28 +0300

    9 task tests added

--- 9.lisp
+++ 9.lisp
@@ -1,7 +1,6 @@
-(print "Hello world")
 (defun srt (lst)
                 (cond 
-                    ((null (car lst)) `())
+                    ((null (car lst)) lst)
                     (t 
                         (setq nlst (srt (cddr lst)))
                         (list
@@ -13,3 +12,7 @@
 )
 
 (print (srt `(1 2 3 4 5 6)))
+(print (srt `(1 nil)))
+(print (srt `(1 2 3)))
+(print (srt `(nil)))
+(print (srt `(hello world this is code test)))
