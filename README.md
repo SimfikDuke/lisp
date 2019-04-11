7aed6500480ee8bd70529e76db66b601f1e2ab68
Simfik Duke <simfikduke@gmail.com>
2019-04-11 09:18:57 +0300

    upper power functions

--- README.md
+++ README.md
@@ -499,20 +499,7 @@
 
 ```lisp
 (defun any-of-p (funcp args)
-	(
-		(lambda (first-arg rest-args)
-			(
-				cond
-				((null args) nil)
-    			((apply funcp (list first-arg)) t)
-				(t
-					(any-of-p funcp rest-args)
-				)
-			)
-		)
-		(car args)
-		(cdr args)
-	)
+		(not (null (mapcan funcp args)))
 )
 ```  
 </p>
@@ -523,7 +510,7 @@
 #### Код на LISP  
 
 ```lisp
-(defun eq-to-two-p (val) (cond ((eq val 2)) (t nil)))
+(defun eq-to-two-p (val) (cond ((eq val 2) (list t)) (t nil)))
 
 (print (any-of-p `eq-to-two-p `(1 3 4 5)))
 (print (any-of-p `eq-to-two-p `(1 3 4 5 2 3)))
--- f_5.lisp
+++ f_5.lisp
@@ -1,23 +1,10 @@
 ;5. Определитефункциональныйпредикат(НЕКОТОРЫй пред список),который истинен, когда, являющейся функциональным аргументом предикат пред истинен хотя бы для одного элемента списка список.
 
-(defun eq-to-two-p (val) (cond ((eq val 2)) (t nil)))
+(defun eq-to-two-p (val) (cond ((eq val 2) (list t)) (t nil)))
 
 (defun any-of-p (funcp args)
-	(
-		(lambda (first-arg rest-args)
-			(
-				cond
-				((null args) nil)
-    			((apply funcp (list first-arg)) t)
-				(t
-					(any-of-p funcp rest-args)
-				)
-			)
-		)
-		(car args)
-		(cdr args)
-	)
-)
+		(not (null (mapcan funcp args)))
+) 
 
 (print (any-of-p `eq-to-two-p `(1 3 4 5)))
 (print (any-of-p `eq-to-two-p `(1 3 4 5 2 3)))
